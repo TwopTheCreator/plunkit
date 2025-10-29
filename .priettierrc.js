65641fa96ca3c681b7d9409e04292551d1f12cb3
twop <tcotland@gmail.com>
2025-10-29 16:52:09 -0400

    Create .priettierrc.js

---
+++ .priettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,                  // Use semicolons
+  singleQuote: true,           // Use single quotes
+  trailingComma: 'all',        // Trailing commas where possible
+  printWidth: 100,             // Maximum line width
+  tabWidth: 2,                 // Indentation width
+  useTabs: false,              // Use spaces instead of tabs
+  bracketSpacing: true,        // Spaces between brackets in object literals
+  arrowParens: 'always',       // Always include parentheses around arrow function params
+  endOfLine: 'lf',             // Line endings
+};
